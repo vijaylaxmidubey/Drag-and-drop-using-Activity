e14b1a12a108f85456b01233f7824f47ecad9f5f
Radhemohan Dubey <radhe.dubey@gmail.com>
2019-09-16 23:10:02 +0530

    created new repository

---
+++ app/src/main/java/com/customer/puzzleapp/MainActivity.java
@@ -0,0 +1,168 @@
+package com.customer.puzzleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.NinePatchDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import com.h6ah4i.android.widget.advrecyclerview.animator.DraggableItemAnimator;
+import com.h6ah4i.android.widget.advrecyclerview.animator.GeneralItemAnimator;
+import com.h6ah4i.android.widget.advrecyclerview.decoration.ItemShadowDecorator;
+import com.h6ah4i.android.widget.advrecyclerview.draggable.RecyclerViewDragDropManager;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements DraggableGridExampleAdapter.ImageMatchedListener {
+    private static final String FRAGMENT_TAG_DATA_PROVIDER = "data provider";
+    private static final String FRAGMENT_LIST_VIEW = "list view";
+    ArrayList<Bitmap> chunkedImages;
+    int chunkNumbers = 9;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private DraggableGridExampleAdapter mAdapter;
+    private RecyclerView.Adapter mWrappedAdapter;
+    private RecyclerViewDragDropManager mRecyclerViewDragDropManager;
+    Bitmap bitmap;
+ /*   public MainActivity() {
+        super();
+    }*/
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+     bitmap  = BitmapFactory.decodeResource(getResources(),R.drawable.dog);
+
+        splitImage(bitmap,chunkNumbers);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mLayoutManager = new GridLayoutManager(getApplicationContext(), 3, RecyclerView.VERTICAL, false);
+
+        // drag & drop manager
+        mRecyclerViewDragDropManager = new RecyclerViewDragDropManager();
+        mRecyclerViewDragDropManager.setItemMoveMode(RecyclerViewDragDropManager.ITEM_MOVE_MODE_SWAP);
+        mRecyclerViewDragDropManager.setDraggingItemShadowDrawable(
+                (NinePatchDrawable) ContextCompat.getDrawable(getApplicationContext(), R.drawable.material_shadow_z3));
+        // Start dragging after long press
+        mRecyclerViewDragDropManager.setInitiateOnLongPress(true);
+        mRecyclerViewDragDropManager.setInitiateOnMove(false);
+        mRecyclerViewDragDropManager.setLongPressTimeout(50);
+
+        // setup dragging item effects (NOTE: DraggableItemAnimator is required)
+        mRecyclerViewDragDropManager.setDragStartItemAnimationDuration(0);
+        mRecyclerViewDragDropManager.setDraggingItemAlpha(0.8f);
+        mRecyclerViewDragDropManager.setDraggingItemScale(1.0f);
+        mRecyclerViewDragDropManager.setDraggingItemRotation(15.0f);
+
+        //adapter
+        final DraggableGridExampleAdapter myItemAdapter = new DraggableGridExampleAdapter(getDataProvider());
+        mAdapter = myItemAdapter;
+
+        mWrappedAdapter = mRecyclerViewDragDropManager.createWrappedAdapter(myItemAdapter);      // wrap for dragging
+
+        GeneralItemAnimator animator = new DraggableItemAnimator(); // DraggableItemAnimator is required to make item animations properly.
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mWrappedAdapter);  // requires *wrapped* adapter
+        mRecyclerView.setItemAnimator(animator);
+
+        // additional decorations
+        //noinspection StatementWithEmptyBody
+        if (supportsViewElevation()) {
+            // Lollipop or later has native drop shadow feature. ItemShadowDecorator is not required.
+        } else {
+            mRecyclerView.addItemDecoration(new ItemShadowDecorator((NinePatchDrawable) ContextCompat.getDrawable(getApplicationContext(), R.drawable.material_shadow_z1)));
+        }
+
+        mRecyclerViewDragDropManager.attachRecyclerView(mRecyclerView);
+
+        // for debugging
+//        animator.setDebug(true);
+//        animator.setMoveDuration(2000);
+    }
+
+
+
+
+    private boolean supportsViewElevation() {
+        return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP);
+    }
+
+    public AbstractDataProvider getDataProvider() {
+        return new ImageProvider(this,chunkedImages);
+       // return ((MainActivity) getApplicationContext()).getDataProvider();
+    }
+
+
+  /*  public AbstractDataProvider getDataProvider() {
+        //Collections.shuffle(chunkedImages);
+        return new ImageProvider(chunkedImages);
+    }*/
+
+
+    private void splitImage(Bitmap bitmap, int chunkNumbers) {
+
+        //For the number of rows and columns of the grid to be displayed
+        int rows, cols;
+
+        //For height and width of the small image chunks
+        int chunkHeight, chunkWidth;
+
+        //To store all the small image chunks in bitmap format in this list
+        chunkedImages = new ArrayList<Bitmap>(chunkNumbers);
+
+        //Getting the scaled bitmap of the source image
+//        BitmapDrawable drawable = (BitmapDrawable) image.getDrawable();
+//        image.setImageResource(R.drawable.ic_chevron_right_black_24dp);
+//        Bitmap bitmap = drawable.getBitmap();
+
+        Bitmap scaledBitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), true);
+
+        rows = cols = (int) Math.sqrt(chunkNumbers);
+        chunkHeight = bitmap.getHeight() / rows;
+        chunkWidth = bitmap.getWidth() / cols;
+        //xCoord and yCoord are the pixel positions of the image chunks
+        int yCoord = 0;
+        for (int x = 0; x < rows; x++) {
+            int xCoord = 0;
+            for (int y = 0; y < cols; y++) {
+
+                chunkedImages.add(Bitmap.createBitmap(scaledBitmap, xCoord, yCoord, chunkWidth, chunkHeight));
+                xCoord += chunkWidth;
+            }
+            yCoord += chunkHeight;
+        }
+        // mergeImage(chunkedImages);
+
+        // Function of merge the chunks images(after image divided in pieces then i can call this function to combine and merge the image as one)
+        //
+
+
+    }
+
+    @Override
+    public void onImageMatched() {
+        Toast.makeText(getApplication(),"Matched" ,Toast.LENGTH_LONG).show();
+        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.out);
+        mRecyclerView.startAnimation(animFadeIn);
+/*
+
+  Animation animFadeOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.anim);
+        mRecyclerView.startAnimation(animFadeOut);
+*/
+        mRecyclerViewDragDropManager.cancelDrag();
+        mRecyclerViewDragDropManager.setInitiateOnLongPress(false);
+
+    }
+}
